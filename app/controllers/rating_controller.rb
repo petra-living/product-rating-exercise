579d76eb133a6409d1d362161b8a086d22a4aee2
Baker Moran <baker.moran@joinroot.com>
2022-02-05 16:53:34 -0500

    add product rating creation endpoint

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -6,9 +6,28 @@ class RatingController < ApplicationController
     render json: @product_ratings, status: :ok
   end
 
+  def create
+    @new_product_rating = ProductRating.new(
+      :author => create_params[:author],
+      :rating => create_params[:rating],
+      :review_title => create_params[:review_title],
+      :review_body => create_params[:review_body],
+      :product_id => create_params[:id]
+    )
+    if @new_product_rating.save
+      render :json => {
+        :id => @new_product_rating.id
+      }
+    end
+  end
+
   private
 
   def show_params
     params.permit(:id)
+  end
+
+  def create_params
+    params.permit(:author, :rating, :review_title, :review_body, :id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   resources :product, only: [:show, :index]
   get "product/:id/product_ratings" => "rating#index"
+  post "product/:id/product_rating" => "rating#create"
 end
